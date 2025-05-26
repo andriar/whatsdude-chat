0f9a0892cab2ea387a52353e69d38852abca616b
Ochaa <mushoddaqt@gmail.com>
2025-05-26 08:58:24 +0700

    Refactor HeaderBar component for improved layout and styling
    - Updated the HeaderBar component to enhance the layout with better spacing and alignment.
    - Introduced a new HeaderActionButton component for action buttons, improving modularity.
    - Adjusted CSS classes for a more responsive design and better visual hierarchy.

--- components/features/inbox/chat/header-area/HeaderBar.vue
+++ components/features/inbox/chat/header-area/HeaderBar.vue
@@ -1,27 +1,28 @@
 <template>
-  <div class="flex items-center p-6 border-b border-white-300 bg-white">
-    <UAvatar :src="activeConversation.avatar" class="w-10 h-10" />
-    <div class="ml-4.5">
-      <div class="body-sm font-semibold">{{ activeConversation.name }}</div>
-      <div class="text-xs text-green-400">Online</div>
+  <div
+    class="flex items-center justify-between px-4 py-2 border-b border-white-300 bg-white"
+  >
+    <div class="flex items-center gap-x-4 flex-1">
+      <!-- Avatar -->
+      <UAvatar :src="activeConversation.avatar" class="w-10 h-10" />
+
+      <!-- Name and Status -->
+      <div>
+        <div class="text-sm text-black-900 font-semibold">
+          {{ activeConversation.name }}
+        </div>
+        <div class="text-xs text-green-400">Online</div>
+      </div>
     </div>
 
-    <UInput
-      icon="i-lucide-search"
-      placeholder="Search..."
-      class="ml-auto w-44"
-    />
-
-    <div class="flex gap-2 ml-4">
-      <UButton icon="i-lucide-mic" size="sm" variant="ghost" />
-      <UButton icon="i-lucide-paperclip" size="sm" variant="ghost" />
-      <UButton icon="i-lucide-ellipsis-vertical" size="sm" variant="ghost" />
-    </div>
+    <!-- Actions -->
+    <HeaderActionButton />
   </div>
 </template>
 
 <script setup lang="ts">
 import type { IConversation } from '~/types/inbox';
+import HeaderActionButton from './HeaderActionButton.vue';
 
 defineProps<{
   activeConversation: IConversation;
