01ef5c99136e5b47d824520df9043bf2858118bf
Andriar Mafazan Rizkiansah <andriar@qiscus.cx>
2025-06-02 13:16:49 +0700

    Enhance dark mode support and refactor components
    - Updated `EmptyChat.vue` to improve dark mode styling.
    - Refactored `PopUpNewConversation.vue` by removing redundant template code and enhancing user search functionality.
    - Improved type safety in `inbox/index.vue` by specifying types for conversation checks.
    
    Co-authored-by: Andriar Mafazan Rizkiansah <andriar@qiscus.cx>

--- components/empty/EmptyChat.vue
+++ components/empty/EmptyChat.vue
@@ -1,6 +1,6 @@
 <template>
   <section
-    class="flex-1 bg-white/50 backdrop-blur-sm rounded-2xl shadow-md flex flex-col overflow-hidden"
+    class="flex-1 bg-white/50 dark:bg-gray-800/50 backdrop-blur-sm rounded-2xl shadow-md flex flex-col overflow-hidden"
   >
     <div class="flex items-center justify-center h-full">
       <slot />
